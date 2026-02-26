a88e890384538a7cc798a45cc52f9a12d0788cb2
AlessandroHBTSA <alessandro.defilippis1@gmail.com>
2026-02-26 11:30:55 +0100

    chore: update .gitignore and ignore more build artifacts
    - Add common .NET build directories and files (e.g., .vs, *.suo, *.nupkg)
    - Expand Node/Angular patterns to ignore coverage and log files
    - Consolidate patterns for better maintainability

--- BankApi/obj/Debug/net8.0/BankApi.AssemblyInfo.cs
+++ BankApi/obj/Debug/net8.0/BankApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4a396b75e218e6b7d0082e725082987fd13bc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae1cd6f19499bf77048ae5f0a5b046240a738fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
